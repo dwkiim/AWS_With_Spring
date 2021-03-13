b88f0a16f5fd8a071ceda4178a5a72ed600185c6
dwkiim <runner138@naver.com>
2021-03-13 18:08:50 +0900

    chapter 4 and 5

---
+++ src/main/java/com/dongwoo/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,18 @@
+package com.dongwoo.springboot.web.dto;
+import com.dongwoo.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
